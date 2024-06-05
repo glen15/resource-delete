6b48292d605c06fb24102e44ffc77c26a11bef73
Glen <glen910115@gmail.com>
2024-06-05 09:13:26 +0900

    Update delete-codepipeline.py

--- delete-codepipeline.py
+++ delete-codepipeline.py
@@ -1,31 +1,45 @@
 import boto3
 
-### 지워야하는 리소스 키워드###
+### 지워야하는 리소스 키워드 ###
 keyword = "TARGET KEYWORD"
 
 ### 대상 리전 지정 ###
-region_name='TARGET REGION'
+region_name = 'TARGET REGION'
 
-def delete_wsu_resume_codepipelines(keyword):
+def delete_code_pipelines(keyword):
     # 서울 리전 세션 생성
     session = boto3.Session(region_name=region_name)
     
     # CodePipeline 클라이언트 생성
     codepipeline = session.client('codepipeline')
     
-    # 파이프라인 목록 가져오기
+    # CodePipeline 목록 가져오기
     response = codepipeline.list_pipelines()
     
-    print("START")
-    # 이름에 "wsu-resume"가 포함된 파이프라인 찾기
-    for pipeline in response.get('pipelines', []):
-        if keyword in pipeline['name'].lower():  # 소문자로 변환하여 비교
-            print(f"\n\n\nFound Pipeline: {pipeline['name']}")
-            confirmation = input(f"\n\n\nDo you want to delete CodePipeline '{pipeline['name']}'? (y/n): ").strip().lower()
-            if confirmation == 'y':
-                print(f"\n\n\nDeleting Pipeline: {pipeline['name']}")
-                codepipeline.delete_pipeline(name=pipeline['name'])
-    print("\n\n\nEND")
+    print("시작\n\n\n")
+    # 이름에 키워드가 포함된 파이프라인 찾기
+    pipelines_to_delete = [pipeline['name'] for pipeline in response.get('pipelines', []) if keyword in pipeline['name'].lower()]
+    
+    if pipelines_to_delete:
+        print("찾은 CodePipeline 목록:")
+        for pipeline_name in pipelines_to_delete:
+            print(f"- {pipeline_name}")
+        
+        confirmation = input("이 모든 CodePipeline을 삭제하시겠습니까? (y/n): ").strip().lower()
+        if confirmation == 'y':
+            for pipeline_name in pipelines_to_delete:
+                print(f"\n\nCodePipeline 삭제 중: {pipeline_name}")
+                codepipeline.delete_pipeline(name=pipeline_name)
+        else:
+            for pipeline_name in pipelines_to_delete:
+                confirmation = input(f"CodePipeline '{pipeline_name}'을(를) 삭제하시겠습니까? (y/n): ").strip().lower()
+                if confirmation == 'y':
+                    print(f"\n\nCodePipeline 삭제 중: {pipeline_name}")
+                    codepipeline.delete_pipeline(name=pipeline_name)
+    else:
+        print("지정된 키워드가 포함된 파이프라인을 찾을 수 없습니다.")
+    
+    print("\n\n\n종료")
 
 if __name__ == '__main__':
-    delete_wsu_resume_codepipelines(keyword)
+    delete_code_pipelines(keyword)
